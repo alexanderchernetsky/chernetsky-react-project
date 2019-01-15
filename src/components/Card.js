17258d1cd45e96edf2e3fef5aaf25551d3823d5c
Alexander Chernetsky <alexvnderchernetsky@gmail.com>
2019-01-15 18:45:10 +0300

    Create Card component and write styles for it

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Card extends React.Component {
+  handleClick = () => {
+    alert('clicked!');
+  };
+
+  render() {
+    return (
+        <div className="card" onClick={this.handleClick}>
+          <div className="card__picture"></div>
+          <span className="card__caption">Посмотреть меню</span>
+          <p className="card__caption">Сделать заказ</p>
+        </div>
+    )
+  }
+}
+
+export default Card;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import Card from './Card';
 
 function Welcome() {
   return (
       <div className="page-wrapper">
         <h1 className="header">Alexander's pizza</h1>
-        <div className="card">
-          <div className="card__picture"></div>
-          <p className="card__caption">Посмотреть меню</p>
-          <p className="card__caption">Сделать заказ</p>
-        </div>
+        <Card />
       </div>
 
   )
